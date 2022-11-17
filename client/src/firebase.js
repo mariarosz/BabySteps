2d7d4a422cba7c70f46c75e45cb91992f77235cd
Filip Stan <filipsunfire@Filips-MBP.lan>
2022-11-17 15:30:22 +0100

    database semi-done

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,40 @@
+
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  //databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDgW-LsVhFQ4CUCDTe10uEHL1U808kK5JU",
+  authDomain: "babysteps-13f8c.firebaseapp.com",
+  projectId: "babysteps-13f8c",
+  storageBucket: "babysteps-13f8c.appspot.com",
+  messagingSenderId: "621749573096",
+  appId: "1:621749573096:web:d879aebabb873f1d30805d"
 });
 
 export const auth = app.auth();
 export const db = getFirestore(app);
 
 export default app;
+
+
+
+
+
+
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDgW-LsVhFQ4CUCDTe10uEHL1U808kK5JU",
+//   authDomain: "babysteps-13f8c.firebaseapp.com",
+//   projectId: "babysteps-13f8c",
+//   storageBucket: "babysteps-13f8c.appspot.com",
+//   messagingSenderId: "621749573096",
+//   appId: "1:621749573096:web:d879aebabb873f1d30805d"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+
+
+
