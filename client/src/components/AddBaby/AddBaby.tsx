ad1a2238e1ab5c5328eb89477e93fc99f35a1697
Alex <alexander.crump11@gmail.com>
2022-11-17 20:14:11 +0100

    AddBaby refactored to TS

--- client/src/components/AddBaby/AddBaby.tsx
+++ client/src/components/AddBaby/AddBaby.tsx
@@ -3,22 +3,27 @@ import { useAuth } from '../../contexts/AuthContext';
 import { db } from '../../firebase';
 import { collection, addDoc } from 'firebase/firestore';
 import { Confirmation } from '../Confirmation/Confirmation';
+
 import './AddBaby.css'
 
-export function AddBaby({ babyName, setBabyName, setBabyBirth }) {
+export function AddBaby({ babyName, setBabyName, setBabyBirth }: {babyName: string, setBabyName: Function, setBabyBirth: Function}) {
   const { currentUser } = useAuth();
 
   const [confirmation, setConfirmation] = useState(false);
 
   const usersRef = collection(db, 'users');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
 
     const userId = currentUser.uid;
-    const name = event.target.name.value;
+    const target = event.target as typeof event.target & {
+      name: {value: string};
+      date: {value: dateFns};
+    }
+    const name = target.name.value;
+    const date = target.date.value;
     setBabyName(name);
-    const date = event.target.date.value;
     setBabyBirth(date);
 
     addDoc(usersRef, {
@@ -34,8 +39,9 @@ export function AddBaby({ babyName, setBabyName, setBabyBirth }) {
       });
 
     setConfirmation(true);
+    setBabyName('')
+    setBabyBirth('')
 
-    event.target.reset();
   };
 
   return (
