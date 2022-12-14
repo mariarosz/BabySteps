8b52dbf5ea7ec7e50ec0c0cae58d2234bf019916
mariarosz <roszykmaria@gmail.com>
2022-12-14 16:16:47 +0100

    cloudinary env solved

--- client/src/components/UploadWidget/UploadWidget.tsx
+++ client/src/components/UploadWidget/UploadWidget.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react';
 import { HiCheck } from 'react-icons/hi';
+import process from 'process';
 
-export default function UploadWidget({ url, setUrl }:{url: string, setUrl: Function}) {
-  const cloudinaryRef: any = React.useRef(!null)
+export default function UploadWidget({
+  url,
+  setUrl,
+}: {
+  url: string;
+  setUrl: Function;
+}) {
+  const cloudinaryRef: any = React.useRef(!null);
   const widgetRef: any = React.useRef(!null);
 
   useEffect(() => {
-    cloudinaryRef.current = window.cloudinary;
+    cloudinaryRef.current = (window as any).cloudinary;
 
     widgetRef.current = cloudinaryRef.current.createUploadWidget(
       {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
+        cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+        uploadPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
       },
       function (error: string, result: any) {
         if (!error && result && result.event === 'success') {
