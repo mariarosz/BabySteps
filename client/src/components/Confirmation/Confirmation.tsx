503fdab4f23144756585fea1b164576a92d085e3
mariarosz <roszykmaria@gmail.com>
2022-12-14 16:25:53 +0100

    "some more errors fixed"

--- client/src/components/Confirmation/Confirmation.tsx
+++ client/src/components/Confirmation/Confirmation.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import './Confirmation.css'
-
+import './Confirmation.css';
 
 export function Confirmation({ babyName }: { babyName: string }) {
   return (
@@ -8,15 +8,16 @@ export function Confirmation({ babyName }: { babyName: string }) {
       <div id="content-box">
         <h3>Dear {babyName},</h3>
         <p>
-        <span id="text">
-          It's lovely that we can accompany you on this great journey, in your
-          discoveries and achievements.
-        </span>
+          <span id="text">
+            It's lovely that we can accompany you on this great journey, in your
+            discoveries and achievements.
+          </span>
         </p>
         <p>Enjoy every little step of your way!</p>
-        <Link to={'/dashboard'}><button className='btn-get-started'>Let's get started</button></Link>
+        <Link to={'/dashboard'}>
+          <button className="btn-get-started">Let's get started</button>
+        </Link>
       </div>
     </div>
-
   );
 }
